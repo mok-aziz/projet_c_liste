c324df1b02098ce867372e7d4aed989b2e2518ca
Aziz Mokni <mohamedaziz.mokni@etudiant-enit.utm.tn>
2025-12-12 17:46:52 +0100

    Update distributeur.c

--- distributeur.c
+++ distributeur.c
@@ -2,97 +2,154 @@
 #include "bonbon.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-void creerDistributeur(Distributeur tab_dis[],int* nb_dis) {
-    Distributeur d;
+Distributeur* rechercherDistributeur(ListeDistributeurs *liste, int id)
+{
+    NoeudDistributeur *courant=liste->tete;
+    while (courant!=NULL)
+    {
+        if (courant->distributeur.id==id)
+        {
+            return &(courant->distributeur);
+        }
+        courant=courant->suivant;
+    }
+    return NULL;
+}
+
+void creerDistributeur(ListeDistributeurs *liste)
+{
+
+    NoeudDistributeur *nouveau = (NoeudDistributeur*)malloc(sizeof(NoeudDistributeur));
     printf("Saisir l'identifiant du distributeur: ");
-    scanf("%d", &d.id);
-    d.nb_bonbons_actuel=0;
-    tab_dis[*nb_dis]=d;
-    (*nb_dis)++;
+    scanf("%d", &nouveau->distributeur.id);
+    nouveau->distributeur.reserve.tete=NULL;
+    nouveau->distributeur.reserve.taille=0;
+    nouveau->distributeur.nb_bonbons_actuel=0;
+    nouveau->suivant=liste->tete;
+    liste->tete=nouveau;
+    liste->taille++;
+
     printf("Distributeur cree avec succes!\n");
 }
 
-void ajouterBonbon(Distributeur *d) {
-    if(d->nb_bonbons_actuel>=100) {
+void ajouterBonbon(Distributeur *d)
+{
+    if(d->nb_bonbons_actuel>=100)
+    {
         printf("Reserve pleine!\n");
         return;
     }
-
-    Bonbon b;
+    NoeudBonbon *nouveau=(NoeudBonbon*)malloc(sizeof(NoeudBonbon));
     printf("Couleur du bonbon: ");
-    scanf("%s",b.couleur);
+    scanf("%s",nouveau->bonbon.couleur);
     printf("Valeur du bonbon: ");
-    scanf("%d",&b.valeur);
+    scanf("%d",&nouveau->bonbon.valeur);
+    nouveau->suivant=d->reserve.tete;
+    d->reserve.tete=nouveau;
+    d->reserve.taille++;
+    d->nb_bonbons_actuel=d->reserve.taille;
 
-    d->reserve[d->nb_bonbons_actuel]=b;
-    d->nb_bonbons_actuel++;
-    printf("Bonbon ajoute au distributeur!\n");
+    printf("Bonbon ajoutee au distributeur!\n");
 }
 
-void retirerBonbon(Distributeur *d) {
-    if(d->nb_bonbons_actuel==0) {
+void retirerBonbon(Distributeur *d)
+{
+    if(d->nb_bonbons_actuel==0)
+    {
         printf("Distributeur vide!\n");
         return;
     }
 
     char couleur[30];
     printf("Couleur du bonbon a retirer: ");
-    scanf("%s",couleur);
+    scanf("%s", couleur);
 
-    int index=-1;
-    for (int i=0;i<d->nb_bonbons_actuel;i++) {
-        if (strcmp(d->reserve[i].couleur, couleur)==0) {
-            index=i;
-            break;
+    NoeudBonbon *courant=d->reserve.tete;
+    NoeudBonbon *precedent=NULL;
+
+    while (courant!=NULL)
+    {
+        if (strcmp(courant->bonbon.couleur,couleur)==0)
+        {
+
+            if (precedent==NULL)
+            {
+                d->reserve.tete=courant->suivant;
+            }
+            else
+            {
+                precedent->suivant=courant->suivant;
+            }
+            free(courant);
+            d->reserve.taille--;
+            d->nb_bonbons_actuel=d->reserve.taille;
+            printf("Bonbon retire!\n");
+            return;
         }
+        precedent=courant;
+        courant=courant->suivant;
     }
 
-    if (index==-1) {
-        printf("Bonbon non trouve!\n");
-        return;
-    }
-
-    for (int i=index;i<d->nb_bonbons_actuel-1;i++) {
-        d->reserve[i]=d->reserve[i+1];
-    }
-    d->nb_bonbons_actuel--;
-    printf("Bonbon retire!\n");
+    printf("Bonbon non trouve!\n");
 }
 
-void afficherDistributeur(Distributeur d) {
+void afficherDistributeur(Distributeur d)
+{
     printf("\n--- Distributeur %d ---\n",d.id);
     printf("Bonbons actuels: %d\n",d.nb_bonbons_actuel);
-    if(d.nb_bonbons_actuel>0) {
-        printf("Contenu:\n");
-        for(int i=0;i<d.nb_bonbons_actuel;i++) {
-            printf("  ");
-            afficheBonbon(d.reserve[i]);
+
+    if(d.nb_bonbons_actuel>0)
+    {
+        printf("les bonbons:\n");
+        NoeudBonbon *courant=d.reserve.tete;
+        while (courant!=NULL)
+        {
+            printf("\n");
+            afficheBonbon(courant->bonbon);
+            courant=courant->suivant;
         }
     }
 }
 
-void supprimerDistributeur(Distributeur tab_dis[],int* nb_dis) {
+void supprimerDistributeur(ListeDistributeurs *liste)
+{
     int id;
-    printf("ID du distributeur a supprimer: ");
-    scanf("%d", &id);
+    printf(" du distributeur a supprimer: ");
+    scanf("%d",&id);
 
-    int index=-1;
-    for (int i=0;i<*nb_dis; i++) {
-        if (tab_dis[i].id==id) {
-            index=i;
-            break;
+    NoeudDistributeur *courant=liste->tete;
+    NoeudDistributeur *precedent=NULL;
+    while (courant!=NULL) {
+        if (courant->distributeur.id==id)
+            {
+            // Effacer tous les bonbons du distributeur
+            NoeudBonbon *bonbon=courant->distributeur.reserve.tete;
+            while (bonbon!=NULL)
+            {
+                NoeudBonbon *auxBonbon=bonbon;
+                bonbon=bonbon->suivant;
+                free(auxBonbon);
+            }
+
+            // Supprimer le distributeur
+            if (precedent==NULL)
+            {
+                liste->tete=courant->suivant;
+            }
+            else
+            {
+                precedent->suivant=courant->suivant;
+            }
+            free(courant);
+            liste->taille--;
+            printf("Distributeur supprime!\n");
+            return;
         }
+        precedent=courant;
+        courant=courant->suivant;
     }
 
-    if (index==-1) {
-        printf("Distributeur non trouve!\n");
-        return;
-    }
-
-    for (int i=index;i<*nb_dis-1; i++) {
-        tab_dis[i]=tab_dis[i+1];
-    }
-    (*nb_dis)--;
-    printf("Distributeur supprime!\n");
+    printf("Distributeur non trouve!\n");
 }
